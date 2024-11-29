1ac7537c901f7bde65f33b7f852b959c55fda5af
Nazar Drahusevych <drag00sev14.nazar@gmail.com>
2024-11-30 00:49:59 +0200

    Add ITransactionService and implement it in TransactionService with methods for deposit, withdraw and transfer

---
+++ BankingSolution/Interfaces/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using BankingSolution.Dtos;
+
+namespace BankingSolution.Interfaces;
+
+public interface ITransactionService
+{
+    bool Deposit(int accountId, decimal deposit);
+    bool Withdraw(int accountId, decimal withdraw);
+    bool Transfer(int fromAccountId, int toAccountId, decimal amount);
+}
---
+++ BankingSolution/Services/TransactionService.cs
@@ -0,0 +1,21 @@
+﻿using BankingSolution.Interfaces;
+
+namespace BankingSolution.Services;
+
+public class TransactionService : ITransactionService
+{
+    public bool Deposit(int accountId, decimal deposit)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Withdraw(int accountId, decimal withdraw)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Transfer(int fromAccountId, int toAccountId, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
